8ae709e6f62bc4960a8e9ac1b5d2ab64a4843f27
Dan Shauver <dshauver@gmail.com>
2021-08-22 16:14:34 -0500

    Chaning to JSON-ish results dict. input validation

--- job_status.py
+++ job_status.py
@@ -37,20 +37,27 @@ node_status = [] #array for per-node job data from JOB_URI
 node_data = [] #array for dictionary from node_status
 job_duration = [] #array for node job duration data
 start_delay = [] #array for node start delay data
-results = [] #array for specific data to return
 results_dict = {} #dictionary for results FIXME
 
 job_start_time = None #Datestamp for overall job start time
 node_start_time = None #Datestamp for per-node job start time
 
 #Get JOB_ID from first CLI argument
-JOB_ID = sys.argv[1]
-
-#Get Ouput option from first CLI argument
-if sys.argv[2]:
-  OUTPUT = sys.argv[2]
+try:
+  sys.argv[1]
+except:
+  sys.exit("Must specify Job ID")
 else:
-  print("Not output specificied ; defaulting to JSON file")
+  JOB_ID = sys.argv[1]
+
+#Get Ouput option from first CLI argument, or accept default
+try:
+  sys.argv[2]
+except:
+  print("No output specificied ; defaulting to JSON file")
+else:
+  OUTPUT = sys.argv[2]
+  
 
 #Supress warnings about insecure requests.  Using self-signed certs in my environment
 #and don't want to see the errors any more
@@ -59,8 +66,6 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 #Grab the token from the file "token" in the same directory as the script
 with open('token', 'r') as file:
 	PE_TOKEN = file.read().replace('\n', '')
-
-
 
 #Build the job URI from the variables above ; this is in case we ever need to list all jobs,
 #plus it servers as a basis for the specific job in which we're interested
@@ -81,50 +86,21 @@ else:
   sys.exit("\"request.get(",JOB_URI,", verify=False, headers={\"X-Authentication\": <TOKEN>)\" returned status code:", job_status.status_code)
 
 #Add the specific data points we want to return to the results array
-results.append( #Headers for data
-    [
-      "Job ID", #Unique ID of the job
-      "Start Time", #Start time of the job
-      "Completed Time", #Compelted time of the job
-      "Duration" # Overall duration of the job.
-    ]
-)
-
 results_dict["Job ID"] = job_data["name"]
 for k in ['created_timestamp','finished_timestamp','duration','node_count']:
   results_dict[k] = job_data[k]
 results_dict["nodes_finished"] = job_data["node_states"]["finished"]
 
-results.append( #Add data to results array
-  [
-    JOB_ID, #Job ID
-    job_data["created_timestamp"], #Start time of the job
-    job_data["finished_timestamp"], #Completed time of the job
-    job_data["duration"] # Overall duration of the job
-  ]
-)
-
-results.append([""]) #Blank row for separation
-
-results.append( #Header for data
-  [
-    "Connected Nodes", #Total number of nodes on which job is run
-    "Successful Jobs", #Total number of jobs completed successfully
-    "Unsuccessful Jobs", #Total number of jobs not completed successfully
-    "Percent Success" #Percent of per-node jobs successfully completed
-  ]
-)
-results.append( #Add data to results array
-  [
-    job_data["node_count"], #Total number of nodes on which job is run
-    job_data["node_states"]["finished"], #Total number of jobs completed successfully 
-    job_data["node_count"] - job_data["node_states"]["finished"], #Total number of jobs not completed successfully,
-    round(
-      100 * (job_data["node_states"]["finished"] / job_data["node_count"]) #Percent of per-node jobs successfully completed
-    )
-  ]
-)
-results.append([""]) #Blank row for separation
+#results.append( #Add data to results array
+  #[
+    #job_data["node_count"], #Total number of nodes on which job is run
+    #job_data["node_states"]["finished"], #Total number of jobs completed successfully 
+    #job_data["node_count"] - job_data["node_states"]["finished"], #Total number of jobs not completed successfully,
+    #round(
+      #100 * (job_data["node_states"]["finished"] / job_data["node_count"]) #Percent of per-node jobs successfully completed
+    #)
+  #]
+#)
 
 #FIXME Report on # of nodes NOT connected at time of job launch
 
@@ -156,86 +132,17 @@ for i in node_data["items"]:
       node_start_time = time.mktime(datetime.datetime.strptime(i["start_timestamp"], "%Y-%m-%dT%XZ").timetuple()) #convert to datestamp
       start_delay.append(node_start_time - job_start_time) #Add delay between job and node start time to array
 
+#Add percentiles for difference between overall job start time and node job start time to results
 results_dict["start_percentile"] = {}
 for i in [90,50,10] :
   results_dict["start_percentile"][i] = numpy.percentile(start_delay, i) 
 
-#Add percentiles for difference between overall job start time and node job start time to results
-results.append( #Header row
-  [
-    "Node Start Delay Percentiles", #Perentile for node delays
-    "Delay", #Delay between overall job start time and per-node job start time
-    "Units" #Units for data
-  ]
-)
-results.append( #90th percentile data
-  [
-    "90th", #Percentile
-    numpy.percentile(start_delay, 90), #Calculate 90th percentile and add data to array
-    "Seconds" #Units
-  ]
-)
-results.append( #50th percentile data
-  [
-    "50th", #Percentile
-    numpy.percentile(start_delay, 50), #Calculate 50th percentile and add data to array
-    "Seconds" #Calculate 50th percentile and add data to array
-  ]
-)
-results.append( #10th percentile data
-  [
-    "10th", #Percentile
-    numpy.percentile(start_delay, 10), #Calculate 10th percentile and add data to array
-    "Seconds" #Units
-  ]
-)
-results.append([""]) #Blank row for separation
-
+#Add precentile scores for per-node job duration to results.  
 results_dict["job_duration"] = {}
 for i in [90,50,10] :
   results_dict["job_duration"][i] = numpy.percentile(job_duration, i) 
 
-#Add precentile scores for per-node job duration to results.  
-results.append( #Header Row
-  [
-    "Node Job Duration Percentile", #Percentiles for per-node job duration data
-    "Duration", #Duration of per-node jobs
-    "Units" #Units for data
-  ]
-)
-results.append( #90th percentile data
-  [
-    "90th", #Percentile
-    numpy.percentile(job_duration, 90), #Calculate 90th percentile and add data to array
-    "Seconds" #Units
-  ]
-)
-results.append( #50th pecentile data
-  [
-    "50th", #Percentil3
-    numpy.percentile(job_duration, 50), #Calculate 50th percentile and add data to array 
-    "Seconds" #Units
-  ]  
-)
-results.append( #10th percentile data
-  [
-    "10th", #Percentile
-    numpy.percentile(job_duration, 10), #Calculate 10th percentile and add data to array
-    "Seconds" #Units
-  ]
-)
-results.append([""]) #Blank row for separation
-
-#Add the per-node data fields to the results.
-results.append( #Header Row
-  [
-    "Nodename", #Name of node
-    "Start Time", #Start time for node's job run
-    "Finish Time", #Finish time for node's job run
-    "Duration(sec)", #Duration of node's job run
-    "Status" #Completion status of node's job run
-  ]
-)
+#Add per-node job data to results
 results_dict["nodes"] = {}
 for i in node_data["items"]:
   nodename = i["name"]
@@ -243,18 +150,6 @@ for i in node_data["items"]:
   for k in ["start_timestamp","finish_timestamp","duration","state"]:
     results_dict["nodes"][nodename][k] = i[k]  
     
-
-for i in node_data["items"]: #Iterate through nodes
-  results.append( # Add per-node data to results array
-    [
-      i["name"], #Name of node
-      i["start_timestamp"], #Start time for node's job run
-      i["finish_timestamp"], #Finish time for node's job run
-      i["duration"], #Duration of node's job run
-      i["state"] #Completion status of node's job run
-    ]
-  )
-
 if OUTPUT == "console": #if OUTPUT = console, ONLY print to console
   print (json.dumps(results_dict, indent = 2))
 elif OUTPUT == "all": #if OUTPUT = all, write to file and console
