19f195eb917d171edc50547c2f9c1c32dbf652f6
Dan Shauver <dshauver@gmail.com>
2021-08-22 15:53:44 -0500

    Adding output options, JSONish results dict

--- job_status.py
+++ job_status.py
@@ -12,12 +12,12 @@ import requests
 import urllib3
 import urllib.parse
 import numpy
-import tabulate
+import json
 
 #For enhancements to output to files in various formats.
 #import pathlib
 #import socket
-#import json
+#import tabulate
 
 #FIXME Add input validation and usage message
 
@@ -28,7 +28,8 @@ INVENTORY_ENDPOINT = "orchestrator/v1/inventory" #inventory endpoint for invento
 PE_TOKEN = None #Variable to store PE Token
 JOB_ID = None #Variable to store Job ID
 JOBS_URI = None #Full uri for Jobs endpoint ; will build later
-JOB_URI = None #URI for specifi job ; will build later
+JOB_URI = None #URI for specific job ; will build later
+OUTPUT = "json" #Where should the results go?  Defaults to json file & console
 
 job_status = [] #array for job status from JOBS_URI
 job_data = [] #array for dictionary from job_status
@@ -37,9 +38,19 @@ node_data = [] #array for dictionary from node_status
 job_duration = [] #array for node job duration data
 start_delay = [] #array for node start delay data
 results = [] #array for specific data to return
+results_dict = {} #dictionary for results FIXME
 
 job_start_time = None #Datestamp for overall job start time
 node_start_time = None #Datestamp for per-node job start time
+
+#Get JOB_ID from first CLI argument
+JOB_ID = sys.argv[1]
+
+#Get Ouput option from first CLI argument
+if sys.argv[2]:
+  OUTPUT = sys.argv[2]
+else:
+  print("Not output specificied ; defaulting to JSON file")
 
 #Supress warnings about insecure requests.  Using self-signed certs in my environment
 #and don't want to see the errors any more
@@ -49,8 +60,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 with open('token', 'r') as file:
 	PE_TOKEN = file.read().replace('\n', '')
 
-#Get JOB_ID from first CLI argument
-JOB_ID = sys.argv[1]
+
 
 #Build the job URI from the variables above ; this is in case we ever need to list all jobs,
 #plus it servers as a basis for the specific job in which we're interested
@@ -61,6 +71,7 @@ JOB_URI = urllib.parse.urljoin(JOBS_URI, JOB_ID)
 
 #Query the API for Data.  Returns JSON
 job_status = requests.get(JOB_URI, verify=False, headers={"X-Authentication": PE_TOKEN})
+#print(json.dumps(job_status.json(), indent=2)) #print statement for inspecting JSON
 
 #Status of 200 is success ; check for that before proceeding
 #If succcesful, convert the JSON results to python dictionary ; otherwise exit with an error code.
@@ -68,8 +79,6 @@ if job_status.status_code == 200:
   job_data = job_status.json()
 else:
   sys.exit("\"request.get(",JOB_URI,", verify=False, headers={\"X-Authentication\": <TOKEN>)\" returned status code:", job_status.status_code)
-
-#print (json.dumps(job_status.json(), indent=4))
 
 #Add the specific data points we want to return to the results array
 results.append( #Headers for data
@@ -80,6 +89,11 @@ results.append( #Headers for data
       "Duration" # Overall duration of the job.
     ]
 )
+
+results_dict["Job ID"] = job_data["name"]
+for k in ['created_timestamp','finished_timestamp','duration','node_count']:
+  results_dict[k] = job_data[k]
+results_dict["nodes_finished"] = job_data["node_states"]["finished"]
 
 results.append( #Add data to results array
   [
@@ -123,6 +137,8 @@ job_start_time = time.mktime(datetime.datetime.strptime(job_data["created_timest
 #Go get the per-node details.  returns JSON.
 node_status = requests.get(job_data["nodes"]["id"], verify=False, headers={"X-Authentication": PE_TOKEN})
 
+#print(json.dumps(node_status.json(), indent=2)) #print statement for inspecting JSON
+
 #Status of 200 is success ; check for that before proceeding
 #If succcesful, convert the JSON results to python dictionary ; otherwise exit with an error code.
 if node_status.status_code == 200:
@@ -139,6 +155,10 @@ for i in node_data["items"]:
       job_duration.append(i["duration"]) #Add duraction to array
       node_start_time = time.mktime(datetime.datetime.strptime(i["start_timestamp"], "%Y-%m-%dT%XZ").timetuple()) #convert to datestamp
       start_delay.append(node_start_time - job_start_time) #Add delay between job and node start time to array
+
+results_dict["start_percentile"] = {}
+for i in [90,50,10] :
+  results_dict["start_percentile"][i] = numpy.percentile(start_delay, i) 
 
 #Add percentiles for difference between overall job start time and node job start time to results
 results.append( #Header row
@@ -170,6 +190,10 @@ results.append( #10th percentile data
   ]
 )
 results.append([""]) #Blank row for separation
+
+results_dict["job_duration"] = {}
+for i in [90,50,10] :
+  results_dict["job_duration"][i] = numpy.percentile(job_duration, i) 
 
 #Add precentile scores for per-node job duration to results.  
 results.append( #Header Row
@@ -212,6 +236,14 @@ results.append( #Header Row
     "Status" #Completion status of node's job run
   ]
 )
+results_dict["nodes"] = {}
+for i in node_data["items"]:
+  nodename = i["name"]
+  results_dict["nodes"][nodename] = {}
+  for k in ["start_timestamp","finish_timestamp","duration","state"]:
+    results_dict["nodes"][nodename][k] = i[k]  
+    
+
 for i in node_data["items"]: #Iterate through nodes
   results.append( # Add per-node data to results array
     [
@@ -223,6 +255,19 @@ for i in node_data["items"]: #Iterate through nodes
     ]
   )
 
-#FIXME Add options to output to csv, JSON format files
-#FIXME This should be an IF loop, defaulting to console, with console|csv|JSON|all as cli options
-print (tabulate.tabulate(results, tablefmt="grid")) #Print results to console as grid format table
+if OUTPUT == "console": #if OUTPUT = console, ONLY print to console
+  print (json.dumps(results_dict, indent = 2))
+elif OUTPUT == "all": #if OUTPUT = all, write to file and console
+  print (json.dumps(results_dict, indent = 2))
+  #FIXME add writing to JSON, CSV files
+  print ("FIXME ouput to file.csv")
+  print ("FIXME ouput to file.csv")
+elif OUTPUT == "json": #Write JSON to file
+  #FIXME add writing JSON to file
+  print ("FIXME ouput to file.json")
+elif OUTPUT == "csv": #write CSV to file
+  #FIXME add CSV output to file
+  print ("FIXME ouput to file.csv")
+
+#print (tabulate.tabulate(results, tablefmt="grid")) #Print results to console as grid format table
+#print (tabulate.tabulate(results, tablefmt="grid")) #Print results to console as grid format table
